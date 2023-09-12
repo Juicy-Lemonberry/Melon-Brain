fcb92f963d52c476351ba12349d7a1c3c17ecfda
Jun Wei <khoojunwei6671@gmail.com>
2023-09-12 12:05:44 +0800

    Updated Search Bar and Simple Filtering

--- melon-client/app/page.tsx
+++ melon-client/app/page.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer";
 
 import HorizontalBlogCard from "@/components/HorizontalBlogCard";
 
-import {Box, Container, Typography} from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 
 const styles = {
   hero: {
@@ -35,13 +35,19 @@ const styles = {
     paddingTop: 5,
     paddingBottom: 5,
     display: 'flex',
-    justifyContent: 'center', 
-    alignItems: 'center',     
-    textAlign: 'center',     
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
   },
+  recentBlog:{
+    fontWeight: 800,
+    paddingTop: 3,
+    paddingBottom: 1,
+  }
+  ,
 
-  horizontalCard:{
-    marginTop:5
+  horizontalCard: {
+    marginTop: 5
   }
 };
 
@@ -49,24 +55,47 @@ export default function Home() {
   return (
     <>
 
-      <TopNavbar/>    
-      <Box  sx={styles.hero}>Melon Brain</Box>
+      <TopNavbar />
+      <Box sx={styles.hero}>Melon Brain</Box>
       <Typography variant="h4" sx={styles.blogTitle}>
-          Trending Blogs
-        </Typography>
+        Trending Blogs
+      </Typography>
       <Container sx={styles.blogsContainer}>
-    
-        <BlogCard/>  
-        <BlogCard/>  
-        <BlogCard/> 
-        <Typography variant="h4" sx={styles.blogTitle}>
+
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <div >
+        <Typography variant="h4" sx={styles.recentBlog} >
           Recent Blogs
         </Typography>
+   
+        <div className="flex items-center"> 
+            <p className="mr-2 prose-lg">Catergory:</p>
+            <button className="btn btn-outline glass btn-sm">Food</button>
+            <button className="btn btn-outline glass btn-sm">Lifestyle</button>
+            <button className="btn btn-outline glass btn-sm ">Technology</button>
+            </div>
+          </div>
+          <div className="flex items-center"> 
+          <div className="join">
+  <div>
+    <div>
+      <input className="input input-bordered join-item bg-white text-black" placeholder="Melon Brain"/>
+    </div>
+  </div>
+  <div className="indicator">
+    <button className="btn join-item input-bordered bg-white text-black">Search</button>
+  </div>
+</div>
+</div>
+        
+
         <HorizontalBlogCard />
-        <HorizontalBlogCard/>
-        <HorizontalBlogCard/>
+        <HorizontalBlogCard />
+        <HorizontalBlogCard />
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
 }
