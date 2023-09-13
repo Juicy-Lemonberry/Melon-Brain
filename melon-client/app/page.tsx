dcda7cf9ccf3b53dea104cb30cd59551407ee400
Amabel <80372683+Amabellzq@users.noreply.github.com>
2023-09-13 14:19:25 +0800

    added aboutpage

--- melon-client/app/page.tsx
+++ melon-client/app/page.tsx
@@ -1,10 +1,13 @@
 import TopNavbar from "@/components/TopNavbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <TopNavbar/>    
       <p>For Jun Wei to Work with...</p>
+      
+      <Footer/>
     </>
   );
 }
