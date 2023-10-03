9fc3e10813f22c8ffadef25b4c13fb3eab804182
Yeo Wen Qin <wqyeo@outlook.com>
2023-10-03 15:41:40 +0800

    Added icons for cars

--- singadrive-web-client/components/RentalMapView.tsx
+++ singadrive-web-client/components/RentalMapView.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import config from '@/config';
+import carIcons from '@/utils/carMapIcons';
 
 interface Car {
   location: [number, number];
@@ -11,13 +12,15 @@ interface Car {
 }
 
 const RentalMapView: React.FC = () => {
-  const [carLocations, setCarLocations] = useState<Car[]>([]);
+  // Static map config.
   const mapStartPosition: [number, number] = [1.3521, 103.8198];
   const maxBounds: [[number, number], [number, number]] = [
     [1.16, 103.59],
     [1.47, 104.04]
   ];
-
+  
+  // Fetch location of cars from server after map load.
+  const [carLocations, setCarLocations] = useState<Car[]>([]);
   useEffect(() => {
     fetch(config.API_BASE_URL + '/api/rent-map/rentable-cars')
       .then((response) => response.json())
@@ -29,7 +32,7 @@ const RentalMapView: React.FC = () => {
 
   return (
     <MapContainer 
-      center={mapStartPosition} 
+      center={mapStartPosition}
       zoom={13} 
       style={{ height: '100%', width: '100%' }}
       maxBounds={maxBounds}  
@@ -40,7 +43,8 @@ const RentalMapView: React.FC = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       {carLocations.map((car: Car, index: number) => (
-        <Marker key={index} position={car.location}>
+        // TODO: Different car icons (color) depending on car statuses
+        <Marker key={index} position={car.location} icon={carIcons.green}>
           <Popup>
             {`Name: ${car.name}, Model: ${car.model}`}
           </Popup>
