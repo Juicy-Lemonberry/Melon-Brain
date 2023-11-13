3210f95e2d6835e946c4075bfc2be453db0dda2e
Yeo Wen Qin <wqyeo@outlook.com>
2023-11-13 15:39:28 +0800

    Backend route to fetch all posts for a given category

--- singadrive-web-server/modules/forum/category.js
+++ singadrive-web-server/modules/forum/category.js
@@ -12,6 +12,7 @@ const postgresPool = new Pool({
     port: 5432
 });
 
+const PostContentModel = require('../../mongo_models/forum/postContentModel');
 const mongoConfig = {
     url: `mongodb://127.0.0.1:27017/${process.env.MONGODB_DB}`,
     dbName: `${process.env.MONGODB_DB}`
@@ -35,5 +36,93 @@ router.get("/get-categories", async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+
+//#region Get Category Posts
+
+const MiniPostInformation = require('../../models/miniPostInformation');
+
+async function fetchPostsPostgreSQL(categoryID){
+    const client = await postgresPool.connect();
+    let query = `SELECT * FROM "forum"."get_category_posts"($1);`;
+    const queryResult = await client.query(query, [categoryID]);
+    client.release();
+
+    if (queryResult.rows.length <= 0) {
+        return [];
+    }
+
+    return queryResult.rows;
+}
+
+async function fetchPostContents(postID){
+    const result = await PostContentModel.findOne({
+        id: postID
+    });
+
+    return result;
+}
+
+function trimPostContent(content) {
+    // NOTE: Always make sure its below certain number of chars
+    // if was originally more than some characters,
+    // replace the last 3 characters to '...'.
+    const targetCharacterCount = 40;
+    if (content.length > targetCharacterCount) {
+        return content.substring(0, targetCharacterCount - 3) + '...';
+    }
+    return content;
+}
+
+router.post("/get-posts", async (req , res) => {
+    const categoryID = req.body.category_id;
+    if (categoryID == null){
+        res.status(400).send({ message: 'MISSING FIELDS' });
+        return;
+    }
+
+    try {
+        await mongoose.connect(mongoConfig.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        let miniPosts = []
+
+        // Create mini-post information for each row...
+        const rowResults = await fetchPostsPostgreSQL(categoryID);
+        for (let i = 0; i < rowResults.length; ++i){
+            const currentRow = rowResults[i];
+            const postID = currentRow.post_id;
+            const currentPost = new MiniPostInformation(
+                postID,
+                currentRow.display_name,
+                currentRow.tags,
+                currentRow.created_at,
+                currentRow.last_activity
+            );
+
+            const postContent = await fetchPostContents(postID);
+            if (postContent == null){
+                // TODO: Handling or better logging...
+                console.log(`Post ID of ${postID} is empty at MongoDB!!`);
+                continue;
+            }
+            currentPost.populatePost(postContent.title, trimPostContent(postContent.content));
+
+            miniPosts.push(currentPost.toDictionaryObject());
+        }
+
+        res.status(200).json({"posts": miniPosts});
+    } catch (error) {
+        console.log("Error occured trying to fetch posts in category.\n", error)
+        res.status(500).json({ message: 'Internal Server Error' });
+    } finally {
+        if (mongoose.connection.readyState) {
+            mongoose.connection.close();
+        }
+    }
+});
+
+//#endregion
 
 module.exports = router;
